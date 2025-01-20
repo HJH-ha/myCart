32062a28162d7d74c01680778c6fa1cde1e032c3
JH <wlgud5632@naver.com>
2025-01-20 11:42:12 +0900

    상품페이지

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import HomePage from "./components/Home/HomePage";
 import Navbar from "./components/Navbar/Navbar";
+import ProductsPage from "./components/Products/ProductsPage";
 
 function App() {
   return (
     <>
       <div className="app">
         <Navbar />
-        <HomePage />
+        <main>
+          <ProductsPage />
+          {/* <HomePage /> */}
+        </main>
       </div>
     </>
   );
