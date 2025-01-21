00c86124cd9165078f921ddd41988ceeb4f5b78d
JH <wlgud5632@naver.com>
2025-01-21 10:03:42 +0900

    리액트 훅 폼 사용

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -1,31 +1,41 @@
 import { useRef, useState } from "react";
 import "./LoginPage.css";
+import { useForm } from "react-hook-form";
 
 const LoginPage = () => {
-  //  const passwordRef = useRef(null);
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(user); // 후에 서보로 로그인으로 바꾸며됨
-    setUser({ email: "", password: "" });
-  };
+  const submitData = (formData) => console.log(formData);
+
+  //  const passwordRef = useRef(null);
+  // const [user, setUser] = useState({
+  //   email: "",
+  //   password: "",
+  // });
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(user); // 후에 서보로 로그인으로 바꾸며됨
+  //   setUser({ email: "", password: "" });
+  // };
 
   return (
     <section className="align_center form_page">
-      <form onSubmit={handleSubmit} className="authentication_form">
+      <form onSubmit={handleSubmit(submitData)} className="authentication_form">
         <h2>로그인 폼</h2>
         <div className="form_inputs">
           <div>
             <label htmlFor="email">Email</label>
             <input
-              onChange={(e) => setUser({ ...user, email: e.target.value })}
-              value={user.email}
-              type="email"
-              id="email"
+              // onChange={(e) => setUser({ ...user, email: e.target.value })}
+              // value={user.email}
+              {...register("email")}
+              // type="email"
+              // id="email"
               className="form_text_input"
               placeholder="이메일 입력"
             />
@@ -33,11 +43,12 @@ const LoginPage = () => {
           <div>
             <label htmlFor="password">Password</label>
             <input
-              onChange={(e) => setUser({ ...user, password: e.target.value })}
-              value={user.password}
-              type="password"
+              // onChange={(e) => setUser({ ...user, password: e.target.value })}
+              // value={user.password}
+              {...register("password")}
+              // type="password"
               // ref={passwordRef}
-              id="password"
+              // id="password"
               className="form_text_input"
               placeholder="패스워드"
             />
