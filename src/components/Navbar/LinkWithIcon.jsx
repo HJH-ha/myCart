482262dfe2a460a6e84a55b6a062c1a90278dbc6
JH <wlgud5632@naver.com>
2025-01-22 09:40:40 +0900

    카테고리사이드바 css수정

--- src/components/Navbar/LinkWithIcon.jsx
+++ src/components/Navbar/LinkWithIcon.jsx
@@ -4,7 +4,7 @@ import "./LinkWithIcon.css";
 const LinkWithIcon = ({ title, link, emoji }) => {
   return (
     // 네브링크는 자동으로 active 클래스 추가됨(네브바에서 사용), href => to
-    <NavLink to={link} className="align_center">
+    <NavLink to={link} className="align_center list">
       {title} <img src={emoji} alt="" className="link_emoji" />
     </NavLink>
   );
