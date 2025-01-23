beb63fe9f1439256c3cc95d686a621def338037a
JH <wlgud5632@naver.com>
2025-01-23 12:07:43 +0900

    jwt Decode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 import Navbar from "./components/Navbar/Navbar";
 import Routing from "./components/Routing/Routing";
+import { jwtDecode } from "jwt-decode";
 
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    try {
+      const jwt = localStorage.getItem("token");
+      const jwtUser = jwtDecode(jwt);
+      if (Date.now() >= jwtUser.exp * 1000) {
+        localStorage.removeItem("token");
+        location.reload(); // 재시작(새로고침) , 제일 앞에 window. 생략가능
+      } else {
+        setUser(jwtUser);
+      }
+    } catch (error) {} // 토근이 없는거지 에러는 아님, 그냥 실행
+  }, []);
   return (
     <>
       <div className="app">
-        <Navbar />
+        <Navbar user={user} />
         <main>
           <Routing />
         </main>
