a2fe01489cdc717ca0e12a35e15af21916e5b247
JH <wlgud5632@naver.com>
2025-01-23 14:22:47 +0900

    제품상세페이지 장바구니 추가기능

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import { jwtDecode } from "jwt-decode";
 function App() {
   const [user, setUser] = useState(null);
   const [cart, setCart] = useState([]); // 장바구니
+  const addToCart = (product, quantity) => {
+    setCart([...cart, { product, quantity }]);
+  };
   useEffect(() => {
     try {
       const jwt = localStorage.getItem("token");
@@ -25,7 +28,7 @@ function App() {
       <div className="app">
         <Navbar user={user} cartCount={cart.length} />
         <main>
-          <Routing />
+          <Routing addToCart={addToCart} />
         </main>
       </div>
     </>
