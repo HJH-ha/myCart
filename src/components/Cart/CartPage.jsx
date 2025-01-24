4d10827d23d11a01bea86826c779bcbb9d9ced0b
JH <wlgud5632@naver.com>
2025-01-24 10:06:49 +0900

    카트전체금액계산

--- src/components/Cart/CartPage.jsx
+++ src/components/Cart/CartPage.jsx
@@ -3,9 +3,18 @@ import remove from "../../assets/remove.png";
 import user from "../../assets/user.webp";
 import QuantityInput from "../SingleProduct/QuantityInput";
 import Table from "../Common/Table";
+import { useEffect, useState } from "react";
 
 const CartPage = ({ cart }) => {
-  console.log(cart);
+  // console.log(cart);
+  const [subTotal, setSubTotal] = useState(0);
+  useEffect(() => {
+    let total = 0;
+    cart.forEach((item) => {
+      total += item.product.price * item.quantity;
+    });
+    setSubTotal(total);
+  }, [cart]);
   return (
     <section className="align_center cart_page">
       <div className="align_center user_info">
@@ -41,15 +50,15 @@ const CartPage = ({ cart }) => {
         <tbody>
           <tr>
             <td>총 금액</td>
-            <td>1,200,000 원</td>
+            <td>{subTotal.toLocaleString("KO-KR")}원</td>
           </tr>
           <tr>
             <td>배송비</td>
-            <td>5,000 원</td>
+            <td>3,000 원</td>
           </tr>
           <tr className="cart_bill_final">
             <td>결제금액</td>
-            <td>1,205,000 원</td>
+            <td>{(subTotal + 3000).toLocaleString("KO-KR")}원</td>
           </tr>
         </tbody>
       </table>
